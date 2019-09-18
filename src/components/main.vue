3a9ac5f24c68639f65832a3e58a2c63629e63e5e
NJ90 <48573943+NJ90@users.noreply.github.com>
2019-09-18 17:00:03 +0900

    Initial commit

---
+++ src/components/main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '새로운 사용자 경험을 제공하는 웹퍼블리셔 장나미 입니다.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
