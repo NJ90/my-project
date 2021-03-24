cf45a5db33f8a506709eaa3ea1094d12b17be324
nameejang <jangnamee@gmail.com>
2021-03-24 16:10:30 +0900

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+    data() {
+        return {
+            name: 'NAMEE JANG',
+            skills: 'HTML, CSS, Vue, Javascript, jQuery, Bootstrap, github, Zepline',
+            github: 'https://github.com/NJ90',
+            medium: 'https://nameejang.medium.com/'
+        };
+    },
+    methods:{
+        tab(){
+            
+            
+        }
+
+    }
+}).mount('#portfolio');
--- backup/src/components/HelloWorld.vue
+++ backup/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="title">{{ msg }}</h1>
    
   </div>
 </template>
@@ -16,7 +16,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.title{border-bottom: 5px solid #666;}
 </style>
